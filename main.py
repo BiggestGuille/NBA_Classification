e13ea6846f7251be82a3d343d9522fd8c908a2f4
BiggestGuille <guigrsanti@gmail.com>
2024-04-03 19:33:02 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.modules import database
+from src.modules import data_importer
+from src.modules import analytics
+
+def main():
+    database.initialize_database()
+
+
+
+
+if __name__ == "__main__":
+    main()
