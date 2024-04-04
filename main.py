f92eeb37a5412d4b13104c2187f727533a010c17
BiggestGuille <guigrsanti@gmail.com>
2024-04-04 10:56:38 +0200

    Imported Flask and Index.html created

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from flask import Flask, render_template
 from src.modules import database
-from src.modules import data_importer
 from src.modules import analytics
 
-def main():
+app = Flask(__name__, template_folder="src/templates")
+
+@app.route('/')
+def home():
     database.initialize_database()
-    analytics.get_analytics(2021)
+    # analytics.get_analytics(2021)
+    clasificaciones = "clasificaciones" # PlaceHolder
+    return render_template('index.html', clasificaciones=clasificaciones)
 
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
