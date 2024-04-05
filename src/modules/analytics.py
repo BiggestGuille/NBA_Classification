893184545242a843beb83de5d0c762a71543ecdf
BiggestGuille <guigrsanti@gmail.com>
2024-04-05 17:23:03 +0200

    Analytics to get normal classification of a season

--- src/modules/analytics.py
+++ src/modules/analytics.py
@@ -1,7 +1,16 @@
 from . import database
 
 def get_analytics(season: int):
-    normal_clasification = database.get_normal_classification(season)
-    print(normal_clasification)
-    new_clasification = 0
+    # Si la tabla de clasificaciones normal no existe, se calcula y se crea
+    if database.verify_table("nba_normal_classification") == False:
+        # Se calcula la clasificación normal de la temporada usando la tabla de partidos
+        normal_classification = database.calculate_normal_classification(season)
+        # Se crea la tabla de clasificación normal
+        database.create_normal_classification_table(normal_classification, season)
+
+    # Se obtiene la clasificación normal de la temporada para su posterior utilización
+    data_normal_classification = database.get_normal_classification(season)
+    # print(data_normal_classification)
+
+    new_classification = 0
     
--- src/modules/database.py
+++ src/modules/database.py
@@ -3,12 +3,12 @@ import os
 from . import data_importer
 
 
+# Función para obtener la conexión a la base de datos
 def get_database_connection():
     ruta_db = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'nba_data.db')
     return sqlite3.connect(ruta_db)
 
-
-
+# Función para inicializar la base de datos (primeras tablas importadas del excel si es necesario)
 def initialize_database():
     with get_database_connection() as db:
         cursor = db.cursor()
@@ -21,9 +21,53 @@ def initialize_database():
         else:
             print("Database already exists")
 
+# Función para verificar si una tabla existe en la base de datos
+def verify_table(table_name):
+    with get_database_connection() as db:
+        cursor = db.cursor()
+        # Verificar si la tabla existe
+        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+        if cursor.fetchone():
+            # La tabla existe
+            print(f"La tabla '{table_name}' ya existe.")
+            return True
+        else:
+            # La tabla no existe
+            return False
+
+# Función para crear la tabla de clasificación normal en la base de datos
+# normal_classification es un diccionario con el Id del equipo y el porcentaje de victorias
+def create_normal_classification_table(normal_classification, season):
+    with get_database_connection() as db:
+        # Crear la tabla
+        cursor = db.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nba_normal_classification (
+            "Team Id" INTEGER PRIMARY KEY,
+            Season INTEGER NOT NULL,
+            "Victory Percentage" REAL NOT NULL,
+            FOREIGN KEY("Team Id") REFERENCES nba_teams(id)
+        );
+        ''')
+        db.commit()
+        print(f"La tabla  ha sido creada con éxito.")
+
+        # Introducir los datos
+        # Insertar o actualizar los datos en la tabla
+        for team_id, percentage in normal_classification.items():
+            # Sentencia SQL para insertar cada fila
+            query = "INSERT INTO nba_normal_classification ([Team Id], season, [Victory Percentage]) VALUES (?, ?, ?)"
+            data = (team_id, season, percentage)
+            # Ejecutar la consulta
+            cursor.execute(query, data)
+            db.commit()
+
+        print(f"Los datos han sido insertados con éxito.")
 
 
-def get_normal_classification(season: int):
+# Función para CALCULAR la clasificación normal de la temporada dados los partidos de la misma
+# Devuelve un diccionario con el Id del equipo y el porcentaje de victorias
+def calculate_normal_classification(season: int):
     with get_database_connection() as db:
         cursor = db.cursor()
         query = """
@@ -38,10 +82,15 @@ def get_normal_classification(season: int):
         results = cursor.fetchall()
         
         # Calculate the victory percentage for each team
-        victory_percentage = {team: (victories / total_matches) * 100 for team, victories, total_matches in results}
-        
+        victory_percentage = {team: (victories / total_matches) for team, victories, total_matches in results}
+
         return victory_percentage
 
+# Función para obtener la clasificación normal de la temporada
+def get_normal_classification(season: int):
+    with get_database_connection() as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM nba_normal_classification WHERE season = ?", (season,))
+        data_normal_classification = cursor.fetchall()
+    return data_normal_classification
 
-#for equipo, porcentaje in porcentaje_victorias.items():
- #   print(f"{equipo}: {porcentaje:.2f}% de victorias")
