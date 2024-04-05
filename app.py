9d2d41777be55b8b185b8e3ace510fff4dfd92c8
BiggestGuille <guigrsanti@gmail.com>
2024-04-05 19:19:36 +0200

    Error 404 and Base.html Management

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from src.modules import database
 from src.modules import analytics
 
-app = Flask(__name__, template_folder="src/templates")
+app = Flask(__name__, template_folder="src/templates",static_folder='src/static')
 
 @app.route('/')
 def home():
@@ -11,7 +11,10 @@ def home():
     clasificaciones = "clasificaciones" # PlaceHolder
     return render_template('index.html', clasificaciones=clasificaciones)
 
-
+# Manejador de error para el error 404 (Página no encontrada)
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
