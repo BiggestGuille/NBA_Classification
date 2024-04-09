a76a15d0e9c116c68c80e0a1941770f283c3ddbe
BiggestGuille <guigrsanti@gmail.com>
2024-04-09 12:12:58 +0200

    App updated so that localhost works

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def page_not_found(error):
     return render_template('not_found.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
